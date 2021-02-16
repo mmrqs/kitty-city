8edfe33d51ed6416b66fa5b105bda266c8b4a87d
Mélanie M <marquesmelanie5@gmail.com>
2021-02-16 13:36:39 +0100

    initial commit
    - city
    - import cats
    - cats following rabbit
    - import rabbit
    - basic rabbit moves

---
+++ project/Assets/RabbitCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RabbitCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // Hit by a cat
+        if(collisionInfo.collider.name == "cat(Clone)")
+        {
+            Debug.Log("hit");
+        }
+    }
+}
